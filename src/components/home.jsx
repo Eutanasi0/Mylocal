7535e1540e6d9bc929be4805f557eb357c6ece22
Eutanasi0 <benjaminflores4e@gmail.com>
2024-07-26 00:25:34 -0500

    Update home page description to include local music scene

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,7 +2,7 @@ import './home.css';
 const Home = () => {
     return ( <div>
         <h1 className="titulo">PeMusic</h1>
-        <p className='description'>know about your local</p>
+        <p className='description'>know about your local music scene</p>
     </div> 
         )
 };
